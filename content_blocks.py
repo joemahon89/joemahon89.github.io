554654ad1d3752094ec1a39a91e841e3d90f52a2
jmahon <joemahon89@gmail.com>
2021-01-26 21:43:18 +0000

    Updated blocks and templater

---
+++ content_blocks.py
@@ -0,0 +1,82 @@
+
+
+
+# Home content
+home_content_blocks = [
+	'''
+	This site exists in lieu of a facebook/linkedin page as I'm not a fan of 
+	either. It's mostly a place for me to chuck projects I'm working 
+	on or hobbies.
+	''',
+	'''
+	I do lots of programming in my job but also do it as a hobby. I have a 
+	particular interest in game development and procedural 
+	generation/programmatically generated art. I am an amateur potter and 
+	mainly make slab-built geometric pots. I also collect 
+	<a href="testlink">fossils</a>.
+	'''
+					]
+
+
+# Programming content
+programming_content_blocks = [
+	'''
+	Here are some of my hobby programming projects. Some have source 
+	available on <a href="https://gitlab.com/jmahon">GitLab</a> or 
+	<a href="https://github.com/joemahon89/">GitHub</a>, but 
+	if not its likely because the code is a bit messy. If you 
+	are interested in something where the source isn't public let me 
+	know and I can likely neaten it up enough to share! 
+	''',
+	'''
+	<ul>
+		<li><a href="#procbirds">Procedural birds Twitter bot</a> ( art 🎨 / procedural generation 🔄 / bots 🤖 )</li>
+		<li><a href="#blenderface>">Blender face extractor</a> ( ceramics 🏺 / 3D modeling 🎨 )</li>
+	</ul>
+	'''
+
+					]
+
+# Ceramics content
+ceramics_content_blocks = [
+
+
+
+
+
+
+
+
+
+
+
+
+					]
+
+
+fossils_content_blocks = [
+	'''
+	I enjoy looking for fossils. The most productive area somewhat close to 
+	me is the East/North Yorkshire coast. Some locations on this coastline 
+	are productive for Jurassic fossils like 
+	<a href="#ammonites">ammonites</a>, <a href="#belemnites">belemnites</a> 
+	(squid-like creatures) and <a href="#bivalves">bivalves</a> (shells). 
+	You can also find marine reptile remains (although I have yet to find 
+	any!) and <a href="#jet">Whitby jet</a>. Here are some of my favourite 
+	fossils that I have collected &#129460;
+	'''
+
+
+
+
+
+
+
+
+
+					]
+
+test_url = '<a href=""></a>'
+
+
+
--- templater.py
+++ templater.py
@@ -2,40 +2,70 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from collections import OrderedDict
 
+from content_blocks import (home_content_blocks,
+                            programming_content_blocks,
+                            ceramics_content_blocks,
+                            fossils_content_blocks)
 
+def get_header():
+    header = "Joseph Mahon"
+    return header
+
+def get_pages():
+    pages = OrderedDict()
+    pages["Home"] = {"url":"home.html"}
+    pages["Programming/Gamedev"] =  {"url":"programming.html"}
+    pages["Ceramics"] =  {"url":"ceramics.html"}
+    pages["Fossils"] =  {"url":"fossils.html"}
+    return pages
+
+def get_content(page_title, page_dict):
+    if page_title == "Home":
+        page_content_blocks = home_content_blocks
+    elif page_title == "Programming/Gamedev":
+        page_content_blocks = programming_content_blocks
+    elif page_title == "Ceramics":
+        page_content_blocks = ceramics_content_blocks
+    elif page_title == "Fossils":
+        page_content_blocks = fossils_content_blocks
+    page_dict["content_blocks"] = page_content_blocks
+    return page_dict
+
+
+def save_page():
+    pass
 
 
 def main():
-    # Get the environment
+    # Set up the environment
     env = Environment(
             loader=FileSystemLoader('templates/'),
             autoescape=select_autoescape(['html', 'xml']))
     # Get the template
     template = env.get_template('web.html')
 
-
-    header= "Joseph Mahon"
-
-    top_choices = OrderedDict()
-    top_choices["Home"] = "home.html"
-    top_choices["Programming"] = "projects.html"
-    top_choices["Pottery"] = "test.html"
-
-    test2 = [
-            "bla1",
-            "bla2",
-            "bla4",
-            "bla3",
-            ]
-            
-
-    html_data = template.render(
+    # Get the page headers
+    header = get_header()
+    # Get the different base page dicts (with just urls and titles so far)
+    pages = get_pages()
+    # Get the content for each page
+    for page_title, page_dict in pages.items():
+        # Get the content of the page
+        pages[page_title] = get_content(page_title, page_dict)
+        # Generate the HTML
+        html_data = template.render(
                                 header=header,
-                                top_choices=top_choices,
+                                top_links=pages,
+                                title=page_title,
+                                content_blocks=pages[page_title]["content_blocks"]
                                 )
 
-    with open("completed_template.html", "w") as file:
-        file.write(html_data)
+        with open(page_dict["url"], "w") as file:
+            file.write(html_data)
+
+
+
+
 
 
 
