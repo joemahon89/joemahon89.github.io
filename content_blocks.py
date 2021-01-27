adcdf3142c6f88e0ebc671ea232154dfeb3504c6
jmahon <joemahon89@gmail.com>
2021-01-27 21:56:38 +0000

    More template faffing and content adding

--- content_blocks.py
+++ content_blocks.py
@@ -1,4 +1,13 @@
 
+class ImageBlock():
+	def __init__(self, num_images, image_paths):
+		self.num_images = num_images
+		self.image_paths = image_paths
+
+
+images_lander_larry_1 = ImageBlock(2, ["images/landerlarry01.png", "images/landerlarry02.jpg"])
+images_lander_larry_2 = ImageBlock(2, ["images/landerlarry03.jpg", "images/landerlarry04.jpg", "images/landerlarry05.jpg", ])
+images_foxes_chickens_1 = ImageBlock(2, ["images/foxes00.png", "images/foxes01.png", "images/foxes04.png"])
 
 
 # Home content
@@ -27,15 +36,100 @@ programming_content_blocks = [
 	are interested in something where the source isn't public let me 
 	know and I can likely neaten it up enough to share! 
 	''',
+
 	'''
 	<ul>
-		<li><a href="#procbirds">Procedural birds Twitter bot</a> ( art 🎨 / procedural generation 🔄 / bots 🤖 )</li>
-		<li><a href="#blenderface>">Blender face extractor</a> ( ceramics 🏺 / 3D modeling 🎨 )</li>
-		<li><a href="#foxeschickens>">"Foxes and Chickens" game</a> ( gamedev 🕹 / Android 🤖 )</li>
-		<li><a href="#landerlarry>">"Lander Larry" game</a> ( gamedev 🕹 / Android 🤖 )</li>
-		<li><a href="#miscgames>">Other small miscellaneous prototypes and projects</a></li>
+		<li><a href="#procbirds">"Procedural Birds" Twitter bot</a> ( art 🎨 / procedural generation 🔄 / bots 🤖 )</li>
+		<li><a href="#blenderface">Blender face extractor</a> ( ceramics 🏺 / 3D modeling 🎨 )</li>
+		<li><a href="#foxeschickens">"Foxes and Chickens" game</a> ( gamedev 🕹 / Android 🤖 )</li>
+		<li><a href="#landerlarry">"Lander Larry" game</a> ( gamedev 🕹 / Android 🤖 )</li>
+		<li><a href="#spacecowboy">"Space Cowboy" game (made in <48h for LDJAM)</a> ( gamedev 🕹 )</li>
+		<li><a href="#miscgames">Other small miscellaneous prototypes and projects</a></li>
 	</ul>
+	''',
+
 	'''
+	<h2 id="procbirds">"Procedural Birds" Twitter bot</h2> 
+	I am interested in procedural generation and generative art, where 
+	images are created using systems and rules defined in code. I wrote 
+	a program which generates simple, stylised images of birds. Each image 
+	is created from scratch and is completely unique. Every line, angle, 
+	colour is decided using randomness within defined limits. 
+	''',
+	'''
+	The birds are built line by line, starting at the right-most foot, 
+	drawing the neck length and angle based on the reasonable 
+	approximations of the possible angles that a bird's neck may have. 
+	The head is a random percentage of a full circle between two limits.
+	The beak can attach at numerous points on the head and have variable 
+	thicknesses and colours. The angle of the back varies, and can result 
+	birds ranging in appearance from from a long-necked goose-like bird to 
+	stumpy quails. The tail angle, thickness, colour, number of feathers 
+	is all computed and drawn. The leg length and angle is chosen based on 
+	the approximate center of gravity of the bird, so that it doesn't look 
+	like it will topple over! The colours are chosen from colour schemes 
+	defined in code, and there is a probability that the bird may have 
+	different coloured wings or tail or both.
+	''',
+	'''
+	The program also generates names for the birds based on their features.
+	For example, a bird where the tail has been drawn with short lines may 
+	have the prefix 'short-tailed' or 'stub-tailed'. Birds with larger angles 
+	between the lines on their back may have the prefix 'hump-backed'. The 
+	colours are also used as descriptors, with ornithological sounding 
+	additions, e.g. 'golden-tailed', 'candy winged', 'bronze-feathered'.
+	''',
+	'''
+	I stuck the program onto a Raspberry Pi and wrote a small Twitter bot 
+	which generates a new bird every hour, names it based on its features 
+	and posts the image on Twitter. 
+	''',
+	'''
+	<a href="https://twitter.com/pybirdbot/">
+		Bird bot Twitter account 🐦
+	</a>
+	''',
+	'''
+	<a href="#top">Back to top</a>
+	''',
+
+
+	'''
+	<h2 id="landerlarry">Lander Larry </h2> 
+	This is the first Android game I developed. The player controls 
+	a space ship and navigates from the start of the level to a landing pad 
+	at the end of the level. There are obstacles along the way including 
+	moving lava, gravitational anomalies and falling rocks. The game is free
+	and there are no ads or in-app-purchases. 
+	<a href="https://play.google.com/store/apps/details?id=org.godotengine.landerlarry">
+		Google Play Store
+	</a>
+	''',
+	images_lander_larry_1,
+	images_lander_larry_2,
+	'''
+	<a href="#top">Back to top</a>
+	''',
+
+	'''
+	<h2 id="foxeschickens">Foxes and Chickens </h2> 
+	This is the second Android game that I published. It is a family-friendly 
+	puzzle game, where the aim of the game is to move your chickens into their 
+	coops using a limited number of moves (and without getting eaten by 
+	foxes!). Levels can be completed in 4, 5 or 6 moves depending on 
+	difficulty. There are 60 levels and there is only a single solution to 
+	each level which adds to the challenge. The game is free and there are no 
+	ads or in-app-purchases. 
+	<a href="https://play.google.com/store/apps/details?id=com.a4x8e6.foxesandchickens">
+		Google Play Store
+	</a>
+	''',
+	images_foxes_chickens_1,
+	'''
+	<a href="#top">Back to top</a>
+	''',
+
+
 					]
 
 # Ceramics content
@@ -43,8 +137,8 @@ ceramics_content_blocks = [
 	'''
 	I have been doing ceramics/pottery for a few years. I started with an 
 	evening class once a week, but now just do my own thing and take my pieces 
-	to a local studio for firing. I am still a definite amateur and mostly make 
-	things up as I go along.
+	to a local studio for firing. I am still a definite amateur and mostly 
+	make things up as I go along.
 	''',
 	'''
 	I really like geometric, angular forms and so this is what I make the 
@@ -94,5 +188,11 @@ fossils_content_blocks = [
 
 test_url = '<a href=""></a>'
 
-
+span_inside_header = '''
+						<span>
+							<a href="https://play.google.com/store/apps/details?id=org.godotengine.landerlarry">
+								Free on Google Play Store
+							</a> 
+						</span>
+					'''
 
--- templater.py
+++ templater.py
@@ -5,7 +5,8 @@ from collections import OrderedDict
 from content_blocks import (home_content_blocks,
                             programming_content_blocks,
                             ceramics_content_blocks,
-                            fossils_content_blocks)
+                            fossils_content_blocks,
+                            ImageBlock)
 
 def get_header():
     header = "Joseph Mahon"
@@ -32,6 +33,55 @@ def get_content(page_title, page_dict):
     return page_dict
 
 
+def convert_content(page_dict):
+    """Sometimes will need to change the content of a content block,
+    for example if its some images and need to place them neatly"""
+    cleaned_content_blocks = []
+    for index, content_block in enumerate(page_dict["content_blocks"]):
+        # Images are added as lists
+        if type(content_block) == ImageBlock:
+            image_block_html = create_image_block_html(content_block)
+            cleaned_content_blocks.append(image_block_html)
+        # Text can remain as text, just wrapped in <p> tags
+        else:
+            cleaned_content_blocks.append("<p>" + content_block + "</p>")
+    page_dict["content_blocks"] = cleaned_content_blocks
+    return page_dict
+
+    
+def create_image_block_html(image_content_block):
+    image_content_block_html = []
+    width_per_image = 100 / len(image_content_block.image_paths)
+    image_content_block_html.append('<div class="row">')
+    for image_path in image_content_block.image_paths:
+        image_content_block_html.append(
+                f'<div class="column" style="width:{width_per_image}%">' +
+                f'<img src="{image_path}" alt="{image_path}" style="width:100%">' +
+                f'</div>'
+                )
+    image_content_block_html.append('</div>')
+    image_content_block_html_string = "".join(image_content_block_html)
+    return image_content_block_html_string
+
+'''
+     <div class="row">
+      <div class="column">
+        <img src="images/landerlarry01.png" alt="ll_titlescreen" style="width:100%">
+      </div>
+      <div class="column">
+        <img src="images/landerlarry02.jpg" alt="ll_image1" style="width:100%">
+      </div>
+      <div class="column">
+        <img src="images/landerlarry03.jpg" alt="ll_image2" style="width:100%">
+      </div>
+    </div> 
+
+    ''',
+
+
+
+
+
 def save_page():
     pass
 
@@ -52,6 +102,9 @@ def main():
     for page_title, page_dict in pages.items():
         # Get the content of the page
         pages[page_title] = get_content(page_title, page_dict)
+        # Convert some of the content (get proper HTML for image gallery etc)
+        pages[page_title] = convert_content(page_dict)
+        print(pages[page_title]["content_blocks"])
         # Generate the HTML
         html_data = template.render(
                                 header=header,
